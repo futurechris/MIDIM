d11056f96ae0d4f943f07e78eb7ee1067d6be896
Chris Mills-Price <millspch@gmail.com>
2015-08-20 22:18:02 -0700

    Switched out Sanford.Multimedia.Midi for UnitySynth

---
+++ Assets/Scripts/LoadMidi.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMidi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// load hard-coded file name.
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
