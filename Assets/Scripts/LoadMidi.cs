cb8c09b271738a165adfabe10a103ed599018bd9
Chris Mills-Price <millspch@gmail.com>
2015-08-22 22:11:45 -0700

    Add channel mute/unmute to LoadMidi

--- Assets/Scripts/LoadMidi.cs
+++ Assets/Scripts/LoadMidi.cs
@@ -6,7 +6,7 @@ using CSharpSynth.Sequencer;
 using CSharpSynth.Synthesis;
 using CSharpSynth.Midi;
 
-//[RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(AudioSource))]
 public class LoadMidi : MonoBehaviour {
 	
 	public string testMidiFile = "Midis/Video_Game_Themes_-_Secret_Of_Mana.mid";
@@ -18,7 +18,10 @@ public class LoadMidi : MonoBehaviour {
 	private float gain = 1f;
 	private MidiSequencer midiSequencer;
 	private StreamSynthesizer midiStreamSynthesizer;
-	
+
+
+	private bool allMute = false;
+
 	void Awake()
 	{
 		// For now, imitating the behavior of UnitySynthTest.cs with two goals:
@@ -42,7 +45,12 @@ public class LoadMidi : MonoBehaviour {
 	{
 
 	}
-	
+
+	// Keys:
+	//  P/S: 					Play/Stop
+	//  ` (tilde/backquote):	Mute/Unmute all channels
+	//							If any channel is not muted, this will mute all. Else it unmutes all.
+	//  1-9,0:					Toggle mute for channel <key>
 	void Update()
 	{
 		// add some play/stop keys since I'm not using the GUILayout stuff.
@@ -53,6 +61,49 @@ public class LoadMidi : MonoBehaviour {
 		else if(Input.GetKeyDown(KeyCode.S))
 		{
 			midiSequencer.Stop(true);
+		}
+
+		if(Input.GetKeyDown(KeyCode.BackQuote))
+		{
+			if(allMute)
+			{
+				midiSequencer.UnMuteAllChannels();
+			}
+			else
+			{
+				midiSequencer.MuteAllChannels();
+				allMute = true;
+			}
+		}
+
+		for(int channel=0; channel<10; channel++)
+		{
+			checkSingleMuteKey(channel);
+		}
+	}
+
+	private void checkSingleMuteKey(int channel)
+	{
+		if(channel < 10)
+		{
+			if(Input.GetKeyDown(channel.ToString())){
+				if(midiSequencer.isChannelMuted(channel))
+				{
+					midiSequencer.UnMuteChannel(channel);
+					allMute = false;
+				}
+				else
+				{
+					midiSequencer.MuteChannel(channel);
+					// TODO: Update bookkeeping to so that if you individually mute
+					// 		 each channel, allMute = false ends up true.
+				}
+			}
+		}
+		else
+		{
+			// convert to use QWERTY as 10-15
+			// Maybe ABCDEF would be better?
 		}
 	}
 
@@ -67,7 +118,8 @@ public class LoadMidi : MonoBehaviour {
 	
 	public void MidiNoteOnHandler (int channel, int note, int velocity)
 	{
-
+		// In theory could get data here, but that means we need to play through the song 
+		// to initialize the chain. Weird.
 	}
 	
 	public void MidiNoteOffHandler (int channel, int note)
